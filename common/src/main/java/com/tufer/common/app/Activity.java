8e1c13f5ffd6a16671f5aded546a659a73cbec04
Tufer <1126179195@qq.com>
2019-04-18 00:18:41 +0800

    5.0版本以上可以设置沉浸式状态栏

--- common/src/main/java/com/tufer/common/app/Activity.java
+++ common/src/main/java/com/tufer/common/app/Activity.java
@@ -1,10 +1,14 @@
 package com.tufer.common.app;
 
 import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
 
 
 import com.tufer.common.widget.convention.PlaceHolderView;
@@ -31,6 +35,14 @@ public abstract class Activity extends AppCompatActivity {
             // 得到界面Id并设置到Activity界面中
             int layId = getContentLayoutId();
             setContentView(layId);
+            //5.0版本以上可以设置沉浸式状态栏
+            if (Build.VERSION.SDK_INT >= 21) {
+                Window window = getWindow();
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+                window.setStatusBarColor(Color.TRANSPARENT);
+
+            }
             initBefore();
             initWidget();
             initData();
